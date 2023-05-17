1a064aea8a8ffc4037f7386ff88fab4a30d466b5
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-05-17 18:31:12 +0100

    MVP tasks 1/2

--- src/cake/Cake.js
+++ src/cake/Cake.js
@@ -1,17 +1,13 @@
-const Cake = ({name, price, ingredients, rating}) => {
+const Cake = ({cake}) => {
 
   return (
     <>
         <h2>This is a cake</h2>
           <ul>
-            <li>name: {name}</li>
-            <li>price: {price}</li>
-            <li>ingredients: </li>
-                <ul id="ingredientList">
-                  <li>{ingredients}</li>
-                  
-                </ul>
-            <li>rating: {rating}</li>
+            <li>{cake.cakeName}</li>
+            <li>{cake.price}</li>
+            <li>{cake.ingredients}</li>
+            <li>{cake.rating}</li>
           </ul>
     </>
   )
