793a627087afc89bd1492793cb80c0f2fdc29a52
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-05-17 19:53:13 +0100

    added validation logic to cake form

--- src/cake/CakeForm.js
+++ src/cake/CakeForm.js
@@ -2,50 +2,75 @@
 
 import { useState } from "react";
 
-const CakeForm =({cake, setCake}) => {
+const CakeForm =({cakes, setCakes}) => {
 // declare state
 
     const [cakeName, setCakeName] = useState("")
     const [cakePrice, setCakePrice] = useState("")
     const [cakeRating, setCakeRating] = useState("")
     const [cakeIngredients, setCakeIngredients] = useState([])
+    
+    const handleCheck = () => {
+        let errorMessage = "";
 
+        if (cakes.find((cake) => cakes.cakeName == cakeName)){
+            errorMessage = "Cake already exists";
+        }
+    }
+    
+    const handleFormSubmit = (e) => {
+        //stop default refresh from happening when submitting
+        e.preventDefault();
 
+        if (!handleCheck()){
+            const updatedCakeList = [... cakes];
+            const newCake = {
+                cakeName: cakeName,
+                ingredients: cakeIngredients,
+                rating: cakeRating,
+                price: cakePrice
+            }
+            updatedCakeList.push(newCake);
+        }
+    }  
 
 
     return (
         <>
-        <h2> Cake Form</h2>
-            <input 
-            type="text"
-            name="name"
-            placeholder="Name of Cake"
-            value={cakeName}
-            onChange={(e) => setCakeName(e.target.value)}
-            />
+        <h2> Add a new cake:</h2>
+            <form onSubmit={handleFormSubmit}>
+                <input 
+                type="text"
+                name="name"
+                placeholder="Name of Cake"
+                value={cakeName}
+                onChange={(e) => setCakeName(e.target.value)}
+                />
 
-             <input 
-            type="text"
-            name="price"
-            placeholder="Price"
-            value={cakePrice}
-            onChange={(e) => setCakePrice(e.target.value)}
-            />
+                <input 
+                type="text"
+                name="price"
+                placeholder="Price"
+                value={cakePrice}
+                onChange={(e) => setCakePrice(e.target.value)}
+                />
 
-            <input 
-            type="text"
-            name="rating"
-            placeholder="Rating"
-            value={cakeRating}
-            onChange={(e) => setCakeRating(e.target.value)}
-            />          
-            <input 
-            type="text"
-            name="ingredients"
-            placeholder="ingredients"
-            value={cakeIngredients}
-            onChange={(e) => setCakeIngredients(e.target.value)}
-            />                             
+                <input 
+                type="text"
+                name="rating"
+                placeholder="Rating"
+                value={cakeRating}
+                onChange={(e) => setCakeRating(e.target.value)}
+                />          
+                <input 
+                type="text"
+                name="ingredients"
+                placeholder="ingredients"
+                value={cakeIngredients}
+                onChange={(e) => setCakeIngredients(e.target.value)}
+                />         
+                <input type="submit" value="Submit"/>      
+            </form>              
         </>
 
     );
