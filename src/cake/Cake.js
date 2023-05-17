7d5ccdba3ef112343d7a04e36f30f9ee76ba478e
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-05-17 18:14:40 +0100

    saving changes before major refactoring

--- src/cake/Cake.js
+++ src/cake/Cake.js
@@ -1,8 +1,18 @@
-const Cake = () => {
+const Cake = ({name, price, ingredients, rating}) => {
 
   return (
     <>
         <h2>This is a cake</h2>
+          <ul>
+            <li>name: {name}</li>
+            <li>price: {price}</li>
+            <li>ingredients: </li>
+                <ul id="ingredientList">
+                  <li>{ingredients}</li>
+                  
+                </ul>
+            <li>rating: {rating}</li>
+          </ul>
     </>
   )
 }
--- src/cake/CakeContainer.js
+++ src/cake/CakeContainer.js
@@ -47,13 +47,79 @@ const CakeContainer = () => {
 		]		
 	)
 
-  return (
-    <>
-        <Cake />
-        <Cake />
-        <Cake />
-    </>
-  )
-}
+	// accessing the cake
+	const cake1 = listOfCakes[0];
 
+	// adding new Cakes to a new CakeList
+	const newCakeList = listOfCakes.map(cake => cake);
+	
+	console.log(newCakeList);
+		
+	
+	const displayCakeName = () => {
+		let nameOfCake = ""
+		let priceOfCake = ""
+		let ratingOfCake = ""
+		let ingredientsOfCake = ""
+		listOfCakes.forEach((cake) => {
+			nameOfCake = cake.cakeName;
+			priceOfCake = cake.price;
+			ingredientsOfCake = cake.ingredients;
+			ratingOfCake = cake.rating;
+		});
+		return nameOfCake;
+	}	
+	const displayCakePrice = () => {
+		let priceOfCake = ""
+		listOfCakes.forEach((cake) => {
+			priceOfCake = cake.price;
+		});
+		return priceOfCake;
+	}	
+	const displayCakeRating = () => {
+		let ratingOfCake = "";
+		listOfCakes.forEach((cake) => {
+			ratingOfCake = cake.rating;
+		});
+		return ratingOfCake;
+	}	
+	const displayCakeIngredients = () => {
+		// let newList = document.getElementById("newIngredientList")
+		let ingredientsOfCake = "";
+		listOfCakes.forEach((cake) => {
+			ingredientsOfCake = cake.ingredients;
+		
+			// for(i = 0; i < ingredientsOfCake; i++){
+			// 	const li = document.createElement("li")
+			// 	li.innerText = "hi";
+			// 	newList.appendChild(li);
+			// }
+		});
+		return ingredientsOfCake;
+	}
+
+
+	// accessing the individual cakeName
+		// console.log(listOfCakes[0].cakeName);
+
+		
+  	return (
+		<>
+			<Cake
+				name={displayCakeName()}
+				rating={displayCakePrice()}
+				price = {displayCakeRating()}
+				ingredients= {displayCakeIngredients()}
+				/>
+			<Cake
+				name={displayCakeName()}
+				rating={displayCakePrice()}
+				price = {displayCakeRating()}
+				ingredients= {displayCakeIngredients()}
+				/>
+	
+				{/* /> */}
+		</>
+  	)
+}
 export default CakeContainer;
